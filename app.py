cca17142996bfc5760db0f54c23c4673e1f75620
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-08-30 10:15:08 -0400

    bug fixed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ print('r')
 data = input('pick random r count(y,n)')
 if data == 'y':
 	count = random.randint(100,999)
-	for i in range(count):
+	for i in range(int(count)):
 		print(f'printed r {i} times')
 else:
 	data = input('count to r how many times')
